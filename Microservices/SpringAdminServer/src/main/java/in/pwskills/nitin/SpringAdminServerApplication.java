0fae3c741623f7f1d5e5ffae7510736f17c98aea
Nitin <javabynitin2022@gmail.com>
2024-10-04 21:29:25 +0530

    SpringBoot admin

---
+++ Microservices/SpringAdminServer/src/main/java/in/pwskills/nitin/SpringAdminServerApplication.java
@@ -0,0 +1,16 @@
+package in.pwskills.nitin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminServerApplication.class, args);
+	}
+
+}
